b67471a1df6e83b4869b2f118371862e918414f7
Laci <elwindly@gmail.com>
2016-12-03 20:40:18 +0100

    Add POST /todos route

---
+++ server/models/todo.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo',{
+    text:{
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
+    completedAt:{
+        type:Number,
+        default:null
+    }
+});
+
+module.exports = {Todo};
+
+// var newUser = new User({
+//     email:'elwindly@gmail.com'
+// });
+// newUser.save().then((result)=>{
+//     console.log(result);
+// },(err)=>{
+//     console.log(err);
+// });
+
+// var newTodo = new Todo({
+//     text:'Cook dinner'
+// });
+// newTodo.save().then((doc)=>{
+//     console.log(doc);
+// },(err)=>{
+//     console.log('Unable to save the todo')
+// });
+
+// var newTodo = new Todo({
+//     text:'Learn programming',
+//     completed:false,
+//     completedAt:0
+// });
+
+// newTodo.save().then((doc)=>{
+//     console.log(doc);
+// },(err)=>{
+//     console.log(err);
+// });
