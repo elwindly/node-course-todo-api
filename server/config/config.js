9ffb9ee0249b6e998790ac1f9fcdaf7490e27c99
Laci <elwindly@gmail.com>
2016-12-04 22:03:34 +0100

    ADD config for test, dev and production

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env**********: ',env);
+
+process.env.MONGODB_URI = 'mongodb://elwindly:7ImmuraN500@ds050539.mlab.com:50539/todo';
+
+if(env ==='development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}else if(env ==='test'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://elwindly:7ImmuraN500@ds050539.mlab.com:50539/todo');
+mongoose.connect(process.env.MONGODB_URI);
 module.exports={
     mongoose
 };
