c17e4ff2a11e62a42df648100f16373eb1dfbafb
Laci <elwindly@gmail.com>
2016-12-04 16:31:35 +0100

    Set up app to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://elwindly:7ImmuraN500@ds050539.mlab.com:50539/todo');
 module.exports={
     mongoose
 };
+
